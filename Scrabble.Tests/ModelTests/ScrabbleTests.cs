cfeee30f8d0bc9792f4bb6fa2e8a6615588c6270
Paige Tiedeman <paigetiedeman@gmail.com>
2021-09-23 09:35:05 -0700

    Third test passing
    Co-Authored-By: Anthony McRae <toddmac12@gmail.com>

--- Scrabble.Tests/ModelTests/ScrabbleTests.cs
+++ Scrabble.Tests/ModelTests/ScrabbleTests.cs
@@ -9,7 +9,7 @@ namespace Scrabble.Tests
   public class ScrabbleTests
   {
     [TestMethod]
-    public void scrabbleScore_ReturnsScoreOfLetters_Number1()
+    public void Dictionary_ReturnsScoreOfLetters_Number1()
     {
       Dictionary<char, int> letters = new Dictionary<char, int>(){{'A', 1}, {'E', 1}, {'I', 1}, {'O', 1}, {'U', 1}, 
 		{'L', 1}, {'N', 1}, {'R', 1}, {'S', 1}, {'T', 1}};
@@ -24,8 +24,32 @@ namespace Scrabble.Tests
       Assert.AreEqual(1, letters['S']);
       Assert.AreEqual(1, letters['T']);
     }
+    [TestMethod]
+    public void Dictionary_ReturnsScoreOfLetters_Number2()
+    {
+        Dictionary<char, int> letters = new Dictionary<char, int>(){{'D', 2}, {'G', 2}};
+        Assert.AreEqual(2,letters['D']);
+        Assert.AreEqual(2,letters['G']);
+    }
+    [TestMethod]
+    public void Dictionary_ReturnsScoreOfLetters_Number3()
+    {
+      Dictionary<char, int> letters = new Dictionary<char, int>(){{'B', 3}, {'C', 3}, {'M', 3}, {'P', 3}};
+      Assert.AreEqual(3, letters['B']);
+      Assert.AreEqual(3, letters['C']);
+      Assert.AreEqual(3, letters['M']);
+      Assert.AreEqual(3, letters['P']);
+    }
   }
 }
 
+
 // input:string word
 // output: int score
+// A, E, I, O, U, L, N, R, S, T       1
+// D, G                               2
+// B, C, M, P                         3
+// F, H, V, W, Y                      4
+// K                                  5
+// J, X                               8
+// Q, Z                               10
