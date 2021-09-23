940f979d349084f370d25fc8ba7cb687ac46834e
Paige Tiedeman <paigetiedeman@gmail.com>
2021-09-23 09:46:52 -0700

    Dictionary tests refactored into 1
    Co-Authored-By: Anthony McRae <toddmac12@gmail.com>

--- Scrabble.Tests/ModelTests/ScrabbleTests.cs
+++ Scrabble.Tests/ModelTests/ScrabbleTests.cs
@@ -9,10 +9,9 @@ namespace Scrabble.Tests
   public class ScrabbleTests
   {
     [TestMethod]
-    public void Dictionary_ReturnsScoreOfLetters_Number1()
+    public void Dictionary_ReturnsScoreOfLetters_Number()
     {
-      Dictionary<char, int> letters = new Dictionary<char, int>(){{'A', 1}, {'E', 1}, {'I', 1}, {'O', 1}, {'U', 1}, 
-		{'L', 1}, {'N', 1}, {'R', 1}, {'S', 1}, {'T', 1}};
+      Dictionary<char, int> letters = new Dictionary<char, int>(){{'A', 1}, {'E', 1}, {'I', 1}, {'O', 1}, {'U', 1}, {'L', 1}, {'N', 1}, {'R', 1}, {'S', 1}, {'T', 1}, {'D', 2}, {'G', 2}, {'B', 3}, {'C', 3}, {'M', 3}, {'P', 3}, {'F', 4}, {'H', 4}, {'V', 4}, {'W', 4}, {'Y', 4}, {'K', 5},	{'J', 8}, {'X', 8},	{'Q', 10}, {'Z', 10}};
       Assert.AreEqual(1, letters['A']);
       Assert.AreEqual(1, letters['E']);
       Assert.AreEqual(1, letters['I']);
@@ -23,50 +22,20 @@ namespace Scrabble.Tests
       Assert.AreEqual(1, letters['R']);
       Assert.AreEqual(1, letters['S']);
       Assert.AreEqual(1, letters['T']);
-    }
-    [TestMethod]
-    public void Dictionary_ReturnsScoreOfLetters_Number2()
-    {
-      Dictionary<char, int> letters = new Dictionary<char, int>(){{'D', 2}, {'G', 2}};
-      Assert.AreEqual(2,letters['D']);
-      Assert.AreEqual(2,letters['G']);
-    }
-    [TestMethod]
-    public void Dictionary_ReturnsScoreOfLetters_Number3()
-    {
-      Dictionary<char, int> letters = new Dictionary<char, int>(){{'B', 3}, {'C', 3}, {'M', 3}, {'P', 3}};
+      Assert.AreEqual(2, letters['D']);
+      Assert.AreEqual(2, letters['G']);
       Assert.AreEqual(3, letters['B']);
       Assert.AreEqual(3, letters['C']);
       Assert.AreEqual(3, letters['M']);
       Assert.AreEqual(3, letters['P']);
-    }
-    [TestMethod]
-    public void Dictionary_ReturnsScoreOfLetters_Number4()
-    {
-      Dictionary<char, int> letters = new Dictionary<char, int>(){{'F', 4}, {'H', 4}, {'V', 4}, {'W', 4}, {'Y', 4}};
       Assert.AreEqual(4, letters['F']);
       Assert.AreEqual(4, letters['H']);
       Assert.AreEqual(4, letters['V']);
       Assert.AreEqual(4, letters['W']);
       Assert.AreEqual(4, letters['Y']);
-    }
-    [TestMethod]
-    public void Dictionary_ReturnsScoreOfLetters_Number5()
-    {
-      Dictionary<char, int> letters = new Dictionary<char, int>(){{'K', 5}};
       Assert.AreEqual(5, letters['K']);
-    }
-    [TestMethod]
-    public void Dictionary_ReturnsScoreOfLetters_Number8()
-    {
-      Dictionary<char, int> letters = new Dictionary<char, int>(){{'J', 8}, {'X', 8}};
       Assert.AreEqual(8, letters['J']);
       Assert.AreEqual(8, letters['X']);
-    }
-    [TestMethod]
-    public void Dictionary_ReturnsScoreOfLetters_Number10()
-    {
-      Dictionary<char, int> letters = new Dictionary<char, int>(){{'Q', 10}, {'Z', 10}};
       Assert.AreEqual(10, letters['Q']);
       Assert.AreEqual(10, letters['Z']);
     }
